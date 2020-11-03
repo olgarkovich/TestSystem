06aa52c69f49909718858fb78cbb5c10474dfdb2
Olya <olgarkovich@gmail.com>
2020-11-03 20:33:31 +0300

    Add models

---
+++ TestSystem/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSystem.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Task{ get; set; }
+        public List<string> WrongAnswer { get; set; }
+        public List<string> RightAnswer { get; set; }
+        public int Weight { get; set; }
+        public string Theme { get; set; }
+    }
+}
